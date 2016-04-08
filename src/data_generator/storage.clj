2114547ce652881d509bdf3d929c156db0612606
Samuel Protas <sprotas@rjmetrics.com>
2016-04-08 11:45:45 -0400

    Storage type dispatch more DRY

--- src/data_generator/storage.clj
+++ src/data_generator/storage.clj
@@ -10,69 +10,61 @@
   [[a op b]]
   (list op a b))
 
+(defn- type-check
+  [config & _]
+  (get-in config [:storage :type]))
+
 (defmulti storage-prep
   "Hook for any code that must be run prior to using storage (connection pool setup, etc.)"
   {:arglists '([config])}
-  (fn [config]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti query
   "Returns an item at random from the provided table-name"
   {:arglists '([config table-name])}
-  (fn [config _]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti query-weighted
   "Returns an item from the provided table-name with a weighted probability from values in the provided weighted-field"
   {:arglists '([config table-name weighted-field table-pk-field])}
-  (fn [config _ _ _]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti query-filtered
   "Returns an item at random from the provided table-name filtered with the provided filter-list expression"
   {:arglists '([config table-name filter-list])}
-  (fn [config _ _]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti query-filtered-weighted
   "Returns an item from the provided table-name, filtered with the provided filter-list, weighted form the values in the provided weighted-field"
   {:arglists '([config table-name filter-list weighted-field table-pk-field])}
-  (fn [config _ _ _ _]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti query-all
   "Returns all results from the given table-name"
   {:arglists '([config table-name])}
-  (fn [config _]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti query-all-filtered
   "Returns all results from the given filter-name filtered with the provided filter-list"
   {:arglists '([config table-name filter-list])}
-  (fn [config _ _]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti create-tables
   "Creates tables for models"
   {:arglists '([config])}
-  (fn [config]
-    (get-in config [:storage :type])))
-
+  type-check)
 
 (defmulti drop-virtual-columns
   "Drops virtual columns from storage so they aren't persisted"
   {:arglists '([config])}
-  (fn [config]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti insert
   "Executes provided insert-statement and returns the result"
   {:arglists '([config insert-statement])}
-  (fn [config _ _]
-    (get-in config [:storage :type])))
+  type-check)
 
 (defmulti raw-query
   "Executes a raw query-string on the underlying storage layer"
   {:arglists '([config query-string])}
-  (fn [config _]
-    (get-in config [:storage :type])))
+  type-check)
